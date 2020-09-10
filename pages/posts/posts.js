559b012164556fbf00337e70b53603209b217f07
黄颖 <huangying@ddfox360.com>
2020-09-10 19:04:25 +0800

    h

---
+++ pages/posts/posts.js
@@ -0,0 +1,108 @@
+// pages/posts/posts.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        swiperList: [
+            {
+                url: "/images/iu1.jpg",
+                id: 111
+            },
+            {
+                url: "/images/iu2.jpg",
+                id: 222
+            },
+            {
+                url: "/images/iu3.jpg",
+                id: 333
+            }
+        ],
+        newsList: [
+            {
+                avatar: "/images/hehua.jpg",
+                headline: "时尚圈重磅新闻！韩国超人气明星IU入职韩都衣舍时尚总监",
+                date: "2020年8月17日",
+                newImage: "/images/iu4.jpg",
+                news: "2020年8月17日，韩国超人气明星IU李知恩入职韩都衣舍时尚总监的消息刷屏时尚圈。这是IU官宣GUUCI韩国区品牌大使后，首次跨入中国时尚圈，也是IU首次合作中国时尚品牌，引发时尚界和公众的广泛热议。",
+                star: 99,
+                eyes: 500,
+            },
+            {
+                avatar: "/images/hehua.jpg",
+                headline: "时尚圈重磅新闻！韩国超人气明星IU入职韩都衣舍时尚总监",
+                date: "2020年8月17日",
+                newImage: "/images/iu4.jpg",
+                news: "2020年8月17日，韩国超人气明星IU李知恩入职韩都衣舍时尚总监的消息刷屏时尚圈。这是IU官宣GUUCI韩国区品牌大使后，首次跨入中国时尚圈，也是IU首次合作中国时尚品牌，引发时尚界和公众的广泛热议。",
+                star: 99,
+                eyes: 500,
+            },
+            {
+                avatar: "/images/hehua.jpg",
+                headline: "时尚圈重磅新闻！韩国超人气明星IU入职韩都衣舍时尚总监",
+                date: "2020年8月17日",
+                newImage: "/images/iu4.jpg",
+                news: "2020年8月17日，韩国超人气明星IU李知恩入职韩都衣舍时尚总监的消息刷屏时尚圈。这是IU官宣GUUCI韩国区品牌大使后，首次跨入中国时尚圈，也是IU首次合作中国时尚品牌，引发时尚界和公众的广泛热议。",
+                star: 99,
+                eyes: 500,
+            }
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
